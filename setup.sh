53f79b26e79ca4e6744c62dc528c35e3b286cacd
lolpoppin <88775780+lolpoppin@users.noreply.github.com>
2026-08-04 08:48:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,11 +16,7 @@ mkdir ~/.config/ulauncher/user-themes
 git clone https://github.com/KiranWells/ulauncher-nord/ \
   ~/.config/ulauncher/user-themes/nord
 sudo mv qt6ct.sh /etc/profile.d
-echo "Fixing Nvidia"
-sudo mkdir /etc/systemd/system/display-manager.service.d
-sudo mv 99-systemd-dri-devices.rules /etc/udev/rules.d
-sudo mv 10-wait-for-dri-devices.conf /etc/systemd/system/display-manager.service.d
-echo "Nvidia Fixed"
+echo "To Make lightdm work run fix.sh"
 echo "Reccommed Setting Up chotic aur and enabling multilib"
 echo "Enable the Themes in qt6ct qt5ct ulauncher and xfce settings"
 echo "Reboot Needed"
