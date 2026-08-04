54b55083b0c93f877e42a0b0ad8eaab3aa9014f0
lolpoppin <88775780+lolpoppin@users.noreply.github.com>
2026-08-04 09:13:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,6 +23,10 @@ sudo mkdir /etc/systemd/system/display-manager.service.d
 sudo mv 99-systemd-dri-devices.rules /etc/udev/rules.d
 sudo mv 10-wait-for-dri-devices.conf /etc/systemd/system/display-manager.service.d
 echo "Nvidia Fixed"
+echo "Cleaning Files Deleting archsetup"
+cd
+rm -rf archsetup
+echo "Finished Cleaning Files"
 echo "Reccommed Setting Up chotic aur and enabling multilib"
 echo "Enable the Themes in qt6ct qt5ct ulauncher and xfce settings"
 echo "Reboot Needed"
