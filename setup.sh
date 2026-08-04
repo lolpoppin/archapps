8a0a187f2fe51497e16fe017785cff927f42757b
lolpoppin <88775780+lolpoppin@users.noreply.github.com>
2026-08-04 09:11:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,12 @@ Graphite-kde-theme/install.sh -t nord
 mkdir ~/.config/ulauncher/user-themes
 git clone https://github.com/KiranWells/ulauncher-nord/ \
   ~/.config/ulauncher/user-themes/nord
-chmod +x fix.sh
-./fix.sh
+sudo mv qt6ct.sh /etc/profile.d
+echo "Fixing Nvidia"
+sudo mkdir /etc/systemd/system/display-manager.service.d
+sudo mv 99-systemd-dri-devices.rules /etc/udev/rules.d
+sudo mv 10-wait-for-dri-devices.conf /etc/systemd/system/display-manager.service.d
+echo "Nvidia Fixed"
+echo "Reccommed Setting Up chotic aur and enabling multilib"
+echo "Enable the Themes in qt6ct qt5ct ulauncher and xfce settings"
+echo "Reboot Needed"
