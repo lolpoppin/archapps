1bda655bdfcbd54408a0748b433e9fddad0fa8ab
lolpoppin <88775780+lolpoppin@users.noreply.github.com>
2026-08-04 15:41:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-echo "Installing Packages 1/14"
-sudo pacman -S base-devel fakeroot debugedit nvidia-open nvidia-settings flatpak lightdm-gtk-greeter-settings kitty file-roller nwg-look
+echo "This is just a collection of stuff i like to have on my setup This script is all I made I don't take credit for it"
+echo "Installing Packages And adding chaotic-AUR 1/14"
+bash <(curl -s https://raw.githubusercontent.com/yakuda-stack/Chaotic-AUR-Setup/main/setup.sh)
+sudo pacman -S base-devel fakeroot debugedit nvidia-open nvidia-settings flatpak lightdm-gtk-greeter-settings kitty file-roller nwg-look ulauncher helium-browser-bin qt6ct-kde
 echo "Removing Build in terminal 2/14"
 sudo pacman -Rns xfce4-terminal
 echo "Building paru 3/14"
@@ -7,9 +9,6 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 echo "Downloading paru apps 4/14"
-paru helium-browser-bin
-paru ulauncher
-paru qt6ct-kde
 paru qt5ct-kde
 echo "Going back to the arch setup dir 5/14"
 cd
@@ -39,6 +38,5 @@ echo "Cleaning Files Deleting archsetup 12/14"
 cd
 rm -rf archsetup
 echo "Finished Cleaning Files 13/14"
-echo "Reccommed Setting Up chotic aur and enabling multilib"
 echo "Enable the Themes in qt6ct qt5ct ulauncher and xfce settings"
 echo "Reboot Needed 14/14"
