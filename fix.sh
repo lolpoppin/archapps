b6a012664daf7bb9064722993e2635907d785e3e
lolpoppin <88775780+lolpoppin@users.noreply.github.com>
2026-08-04 09:09:45 +0100

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -4,3 +4,6 @@ sudo mkdir /etc/systemd/system/display-manager.service.d
 sudo mv 99-systemd-dri-devices.rules /etc/udev/rules.d
 sudo mv 10-wait-for-dri-devices.conf /etc/systemd/system/display-manager.service.d
 echo "Nvidia Fixed"
+echo "Reccommed Setting Up chotic aur and enabling multilib"
+echo "Enable the Themes in qt6ct qt5ct ulauncher and xfce settings"
+echo "Reboot Needed"
